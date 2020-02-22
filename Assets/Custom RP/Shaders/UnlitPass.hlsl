cf4eb9a32c48c6a797bcceac1866bb28f1eecb7a
jsjtxietian <jsjtxietian@outlook.com>
2020-02-22 23:35:44 +0800

    bug fix & color

--- Assets/Custom RP/Shaders/UnlitPass.hlsl
+++ Assets/Custom RP/Shaders/UnlitPass.hlsl
@@ -2,23 +2,17 @@
 #define CUSTOM_UNLIT_PASS_INCLUDED
 
 #include "../ShaderLibrary/Common.hlsl"
-
-// float4 UnlitPassVertex (float3 positionOS: POSITION) : SV_POSITION {
-// 	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
-// 	return TransformWorldToHClip(positionWS);
-// }
-
-// float4 UnlitPassFragment () : SV_TARGET{
-//     return 0.0;
-// }
-
-#define UNITY_MATRIX_M unity_ObjectToWorld
-#define UNITY_MATRIX_I_M unity_WorldToObject
-#define UNITY_MATRIX_V unity_MatrixV
-#define UNITY_MATRIX_VP unity_MatrixVP
-#define UNITY_MATRIX_P glstate_matrix_projection
+float4 _BaseColor;
 
 
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
+float4 UnlitPassVertex (float3 positionOS: POSITION) : SV_POSITION {
+	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+	return TransformWorldToHClip(positionWS);
+}
+
+float4 UnlitPassFragment () : SV_TARGET {
+	return _BaseColor;
+}
+
 
 #endif
