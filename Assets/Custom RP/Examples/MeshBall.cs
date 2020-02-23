7c56a1da64b25db3921818bc8518ceef8172693e
jsjtxietian <jsjtxietian@outlook.com>
2020-02-23 15:29:03 +0800

    Cutoff Per Object

--- Assets/Custom RP/Examples/MeshBall.cs
+++ Assets/Custom RP/Examples/MeshBall.cs
@@ -1,39 +1,51 @@
 using UnityEngine;
 
 // spawn a lot of objects when it awakens
-public class MeshBall : MonoBehaviour {
+public class MeshBall : MonoBehaviour
+{
 
-	static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
 
-	[SerializeField]
-	Mesh mesh = default;
+    [SerializeField]
+    Mesh mesh = default;
 
-	[SerializeField]
-	Material material = default;
+    [SerializeField]
+    Material material = default;
 
-	Matrix4x4[] matrices = new Matrix4x4[1023];
-	Vector4[] baseColors = new Vector4[1023];
-	MaterialPropertyBlock block;
+    Matrix4x4[] matrices = new Matrix4x4[1023];
+    Vector4[] baseColors = new Vector4[1023];
+    MaterialPropertyBlock block;
 
 
-	void Awake () {
-		for (int i = 0; i < matrices.Length; i++) {
-			matrices[i] = Matrix4x4.TRS(
-				Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one
-			);
-			baseColors[i] =
-				new Vector4(Random.value, Random.value, Random.value, 1f);
-		}
-	}
+    void Awake()
+    {
+        for (int i = 0; i < matrices.Length; i++)
+        {
+            matrices[i] = Matrix4x4.TRS(
+                Random.insideUnitSphere * 10f,
+                Quaternion.Euler(
+                    Random.value * 360f, Random.value * 360f, Random.value * 360f
+                ),
+                Vector3.one * Random.Range(0.5f, 1.5f)
 
-	void Update () {
-		if (block == null) {
-			block = new MaterialPropertyBlock();
-			block.SetVectorArray(baseColorId, baseColors);
-		}
-		//mesh, sub-mesh index zero, material, matrices array, 
-		//the amount of elements, and property block as arguments
-		Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
-	}
+            );
+            baseColors[i] =
+                new Vector4(
+                    Random.value, Random.value, Random.value,
+                    Random.Range(0.5f, 1f));
+        }
+    }
+
+    void Update()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+            block.SetVectorArray(baseColorId, baseColors);
+        }
+        //mesh, sub-mesh index zero, material, matrices array, 
+        //the amount of elements, and property block as arguments
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
+    }
 
 }
--- Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour {
 	
 	static int baseColorId = Shader.PropertyToID("_BaseColor");
+	
+	//Cutoff Per Object
+	static int cutoffId = Shader.PropertyToID("_Cutoff");
 	static MaterialPropertyBlock block;
 
 	[SerializeField]
 	Color baseColor = Color.white;
+
+	[SerializeField, Range(0f, 1f)]
+	float cutoff = 0.5f;
 
 	// OnValidate gets invoked in the Unity editor when the component is loaded or changed. 
 	void OnValidate () {
@@ -15,6 +21,7 @@ public class PerObjectMaterialProperties : MonoBehaviour {
 			block = new MaterialPropertyBlock();
 		}
 		block.SetColor(baseColorId, baseColor);
+		block.SetFloat(cutoffId, cutoff);
 		GetComponent<Renderer>().SetPropertyBlock(block);
 	}
 
